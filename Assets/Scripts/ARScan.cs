301d698c45e0366f07e0d909b7a9074cf6ee5210
UnMAnU <m.herrera@getaclub.io>
2021-07-24 05:54:48 -0500

    First commit!

---
+++ Assets/Scripts/ARScan.cs
@@ -0,0 +1,34 @@
+﻿// by @unmanuel
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace vr_BAND {
+
+	public class ARScan : MonoBehaviour {
+	    
+		public GameObject introScreen, scanner;
+
+		public UnityEvent OnStart;
+
+		void Start() {
+			scanner.SetActive(false);
+			OnStart?.Invoke();
+		}
+
+	    public void StartScan() {
+	    	introScreen.SetActive(false);
+	    	scanner.SetActive(true);
+	    }
+
+	    public void GotoSite(string url) {
+	    	Application.OpenURL(url);
+	    }
+
+	    public void Quit() {
+	    	Application.Quit();
+	    }
+	}
+}
---
+++ Assets/Scripts/Panel.cs
@@ -0,0 +1,17 @@
+﻿// by @unmanuel
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Panel : MonoBehaviour {
+
+	public bool faceCamera = false;
+    
+    void Update() {
+    	if(faceCamera) {
+			transform.localEulerAngles = -Camera.main.transform.localEulerAngles;
+			transform.LookAt(Camera.main.transform.position);
+    	}
+    }
+}
